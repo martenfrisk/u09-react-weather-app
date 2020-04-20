39481ecfd116e815a32af0b7803f552808579edb
Mårten Frisk <marten.frisk@chasacademy.se>
2020-04-20 08:41:27 +0200

    Updates

---
+++ precache-manifest.49936b80d4b62db62b0e94b52b1934ba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dadfed0dfcddf1b8b187b56e78789513",
+    "url": "/u09-react-weather-app/index.html"
+  },
+  {
+    "revision": "61bd0ba37525a9c5ed30",
+    "url": "/u09-react-weather-app/static/css/main.381ce28e.chunk.css"
+  },
+  {
+    "revision": "89f9a9591414b50311f9",
+    "url": "/u09-react-weather-app/static/js/2.1afca2b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/u09-react-weather-app/static/js/2.1afca2b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61bd0ba37525a9c5ed30",
+    "url": "/u09-react-weather-app/static/js/main.62ace776.chunk.js"
+  },
+  {
+    "revision": "e535b1ad241996090573",
+    "url": "/u09-react-weather-app/static/js/runtime-main.aaed0030.js"
+  },
+  {
+    "revision": "9b574f50c192d1e21359aa058db60eb3",
+    "url": "/u09-react-weather-app/static/media/icon-location.9b574f50.svg"
+  },
+  {
+    "revision": "54ab538fc161b955018caf29c67a7b89",
+    "url": "/u09-react-weather-app/static/media/sunrise.54ab538f.svg"
+  },
+  {
+    "revision": "add16403429dedf3a7a0b95b25790a1d",
+    "url": "/u09-react-weather-app/static/media/sunset.add16403.svg"
+  }
+]);
